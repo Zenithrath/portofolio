516d8c653522d26179bf79dce7594cfcb81bc1fe
zenithrath <djibril.ranggadeja@gmail.com>
2026-07-30 16:16:33 +0800

    fix: quote kepotong - kurangi ukuran text & tambah min-w-0

--- components/portfolio/PortfolioClient.tsx
+++ components/portfolio/PortfolioClient.tsx
@@ -166,7 +166,7 @@ export default function PortfolioClient({
                     <div className="surface-grid pointer-events-none absolute inset-0 opacity-30" />
                     <div className="relative mx-auto grid max-w-7xl items-stretch overflow-hidden lg:grid-cols-[minmax(0,1.12fr)_minmax(0,0.88fr)] lg:gap-10">
                         <ExperienceSection experiences={experiences} />
-                        <div className="grid min-w-0 content-start border-t border-white/[0.12] lg:border-l lg:border-t-0 lg:pl-10">
+                        <div className="grid min-w-0 content-start overflow-hidden border-t border-white/[0.12] lg:border-l lg:border-t-0 lg:pl-10">
                             {hasQuote && <QuoteSection personal={personal} />}
                             <ContactSection
                                 contacts={contacts}
